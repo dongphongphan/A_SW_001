d00ad32cda0468b2066a0fed374c3a0e67ac5d3a
dongphongphan <dongphongphan1989@gmail.com>
2018-01-06 22:49:15 +0700

    06-Jan

--- Andoid/Calculator/app/src/main/java/toto/calculator/MainActivity.java
+++ Andoid/Calculator/app/src/main/java/toto/calculator/MainActivity.java
@@ -8,17 +8,10 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0,btn13,btn20,btn19,btn18,btn17,btnDEL;
+    private Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0,btn13,btnDot,btn20,btn19,btn18,btn17,btnDEL;
     private TextView txtResult,txtDisplay;
     private String txtInput = "" ;
-    private double valueOne = Double.NaN;
-    private double valueTwo;
-    private static final char ADDITION = '+';
-    private static final char SUBTRACTION = '-';
-    private static final char MULTIPLICATION = '*';
-    private static final char DIVISION = '/';
-    private char CURRENT_ACTION;
-
+    private boolean isEqual = false, isDot = false, isOperator = false, isCalculated = false;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,114 +23,240 @@ public class MainActivity extends AppCompatActivity {
         btn0.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
                 txtDisplay.setText(txtDisplay.getText() + "0");
-                txtInput += "0";   }});
+                txtInput += "0";
+                calculation();
+            }
+        });
         btn1.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
+                isDot = false;
+                isOperator = false;
                 txtDisplay.setText(txtDisplay.getText() + "1");
-                txtInput += "1";}});
+                txtInput += "1";
+                calculation();
+            }});
         btn2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                isDot = false;
+                isOperator = false;
                 txtDisplay.setText(txtDisplay.getText() + "2");
-                txtInput += "2";  }});
+                txtInput += "2";
+                calculation();
+            }});
         btn3.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                isDot = false;
+                isOperator = false;
                 txtDisplay.setText(txtDisplay.getText() + "3");
-                txtInput += "3";   }  });
+                txtInput += "3";
+                calculation();}  });
         btn4.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                isDot = false;
+                isOperator = false;
                 txtDisplay.setText(txtDisplay.getText() + "4");
                 txtInput += "4";
+                calculation();
             }});
         btn5.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                isDot = false;
+                isOperator = false;
                 txtDisplay.setText(txtDisplay.getText() + "5");
                 txtInput += "5";
+                calculation();
             }});
         btn6.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                isDot = false;
+                isOperator = false;
                 txtDisplay.setText(txtDisplay.getText() + "6");
                 txtInput += "6";
+                calculation();
             }});
         btn7.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                isDot = false;
+                isOperator = false;
                 txtDisplay.setText(txtDisplay.getText() + "7");
                 txtInput += "7";
+                calculation();
             }});
         btn8.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                isDot = false;
+                isOperator = false;
                 txtDisplay.setText(txtDisplay.getText() + "8");
                 txtInput += "8";
+                calculation();
             }});
         btn9.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                isDot = false;
+                isOperator = false;
                 txtDisplay.setText(txtDisplay.getText() + "9");
                 txtInput += "9";
+                calculation();
+            }});
+        btnDot.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(!isDot) {
+                    txtDisplay.setText(txtDisplay.getText() + ".");
+                    txtInput += ".";
+                    isDot = true;
+                }
             }});
         btn17.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                calculation();
-                txtDisplay.setText(txtDisplay.getText() + " : ");
-                CURRENT_ACTION = DIVISION;
+                if(!isOperator && (!txtInput.isEmpty())) {
+                    isOperator = true;
+                    isCalculated = true;
+                    calculation();
+                    txtDisplay.setText(txtDisplay.getText() + " : ");
+                    txtInput += "/";
+                }
+                Log.i("setOnClickListener", "btn :");
             }});
         btn18.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                calculation();
-                txtDisplay.setText(txtDisplay.getText() + " x ");
-                CURRENT_ACTION = MULTIPLICATION;
+                if(!isOperator && (!txtInput.isEmpty())) {
+                    isOperator = true;
+                    isCalculated = true;
+                    calculation();
+                    txtDisplay.setText(txtDisplay.getText() + " x ");
+                    txtInput += "*";
+                }
+                Log.i("setOnClickListener", "btn x");
             }});
         btn19.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                calculation();
-                txtDisplay.setText(txtDisplay.getText() + " - ");
-                CURRENT_ACTION = SUBTRACTION;
+                if(!isOperator && (!txtInput.isEmpty())) {
+                    isOperator = true;
+                    isCalculated = true;
+                    calculation();
+                    txtDisplay.setText(txtDisplay.getText() + " - ");
+                    txtInput += "-";
+                }
             }});
         btn20.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                calculation();
-                txtDisplay.setText(txtDisplay.getText() + " + ");
-                CURRENT_ACTION = ADDITION;
+                if(!isOperator && (!txtInput.isEmpty())) {
+                    isCalculated = true;
+                    isOperator = true;
+                    calculation();
+                    txtDisplay.setText(txtDisplay.getText() + " + ");
+                    txtInput += "+";
+                }
                 Log.i("setOnClickListener", "btn ADD");
             }});
         btnDEL.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                isOperator = false;
+                isDot = false;
+                isCalculated = false;
                 txtDisplay.setText(null);
                 txtResult.setText(null);
-                valueOne = Double.NaN;
-                valueTwo = 0;
+                txtInput = "";
                 Log.i("setOnClickListener", "btn DEL");
             }});
         btn13.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                calculation();
-                valueTwo = 0;
+                if(!txtInput.isEmpty()){
+                    calculation();
+                    txtDisplay.setText(txtResult.getText());
+                    txtInput = txtResult.getText().toString();
+                    Log.i("setOnClickListener btn = ",txtInput.toString());
+                    isEqual = true;
+                }
             }});
     }
-
     private void calculation(){
-        if(!Double.isNaN(valueOne))
-        {
-            valueTwo = parseString(txtInput.toString());
-            Log.i("LOG value 1: ",Double.toString(valueOne));
-            Log.i("LOG value 2: ",Double.toString(valueTwo));
-            txtInput = "";
-            if(CURRENT_ACTION == ADDITION){
-                valueOne = this.valueOne + valueTwo;}
-            else if(CURRENT_ACTION == SUBTRACTION){
-                valueOne = this.valueOne - valueTwo;}
-            else if(CURRENT_ACTION == MULTIPLICATION){
-                valueOne = this.valueOne * valueTwo;}
-            else if(CURRENT_ACTION == DIVISION){
-                valueOne = this.valueOne / valueTwo;}
-
-            txtResult.setText(Double.toString(valueOne));
-        }
-        else {
-                try {
-                    valueOne = parseString(txtInput.toString());
-                    txtInput = "";
+        if(isCalculated){
+            try{
+                double result = eval(txtInput);
+                String stringResult = String.format((result % 1 == 0 ? "%.0f" : "%.1f"), result);
+                txtResult.setText(stringResult.toString());
             }
-            catch (Exception e){Log.i("BACKTRACE_02: ",txtInput.toString());}
+            catch (Exception e){
+                Log.i("BACKTRACE_EVAL: ",txtInput.toString());
+                txtResult.setText("Invalid expression");}
         }
+    }
+    private double eval(final String str) {
+        return new Object() {
+            int pos = -1, ch;
+            void nextChar() {
+                ch = (++pos < str.length()) ? str.charAt(pos) : -1;
+            }
+            boolean eat(int charToEat) {
+                while (ch == ' ') nextChar();
+                if (ch == charToEat) {
+                    nextChar();
+                    return true;
+                }
+                return false;
+            }
+            double parse() {
+                nextChar();
+                double x = parseExpression();
+                if (pos < str.length()) throw new RuntimeException("Unexpected: " + (char)ch);
+                return x;
+            }
+
+            // Grammar:
+            // expression = term | expression `+` term | expression `-` term
+            // term = factor | term `*` factor | term `/` factor
+            // factor = `+` factor | `-` factor | `(` expression `)`
+            //        | number | functionName factor | factor `^` factor
+
+            double parseExpression() {
+                double x = parseTerm();
+                for (;;) {
+                    if      (eat('+')) x += parseTerm(); // addition
+                    else if (eat('-')) x -= parseTerm(); // subtraction
+                    else return x;
+                }
+            }
+
+            double parseTerm() {
+                double x = parseFactor();
+                for (;;) {
+                    if      (eat('*')) x *= parseFactor(); // multiplication
+                    else if (eat('/')) x /= parseFactor(); // division
+                    else return x;
+                }
+            }
+
+            double parseFactor() {
+                if (eat('+')) return parseFactor(); // unary plus
+                if (eat('-')) return -parseFactor(); // unary minus
+
+                double x;
+                int startPos = this.pos;
+                if (eat('(')) { // parentheses
+                    x = parseExpression();
+                    eat(')');
+                } else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
+                    while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
+                    x = Double.parseDouble(str.substring(startPos, this.pos));
+                } else if (ch >= 'a' && ch <= 'z') { // functions
+                    while (ch >= 'a' && ch <= 'z') nextChar();
+                    String func = str.substring(startPos, this.pos);
+                    x = parseFactor();
+                    if (func.equals("sqrt")) x = Math.sqrt(x);
+                    else if (func.equals("sin")) x = Math.sin(Math.toRadians(x));
+                    else if (func.equals("cos")) x = Math.cos(Math.toRadians(x));
+                    else if (func.equals("tan")) x = Math.tan(Math.toRadians(x));
+                    else throw new RuntimeException("Unknown function: " + func);
+                } else {
+                    throw new RuntimeException("Unexpected: " + (char)ch);
+                }
+
+                if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation
+
+                return x;
+            }
+        }.parse();
     }
     private double parseString(String strNumber){
         if(strNumber != null && strNumber.length() > 0){
@@ -163,6 +282,7 @@ public class MainActivity extends AppCompatActivity {
         btn9= (Button)findViewById(R.id.btn9);
         btn0= (Button)findViewById(R.id.btn0);
         btn13 = (Button)findViewById(R.id.button13);
+        btnDot = (Button)findViewById(R.id.button7);
         btnDEL= (Button)findViewById(R.id.button14);
 
         btn20= (Button)findViewById(R.id.button20); // +
